3e4880509f7d48020d2058c7a22b71bc334d9d01
VihangaDehigaspitiya <rashmikarc@gmail.com>
2024-05-03 16:45:44 +0530

    added comments to the mock  functions

--- tests/mock/function.ts
+++ tests/mock/function.ts
@@ -1,6 +1,10 @@
 import { TodoList } from "../../src/models/todoList.model";
 import { User } from "../../src/models/user.model";
 
+/**
+ * Remove test users
+ * @returns 
+ */
 export const removeTestUsers = async () => {
   return await User.destroy({
     where: {
@@ -9,10 +13,19 @@ export const removeTestUsers = async () => {
   });
 };
 
+/**
+ * Add test users
+ * @param payload 
+ * @returns 
+ */
 export const addTestUser = async (payload: any) => {
   return await User.create(payload);
 };
 
+/**
+ * Remove todo lists
+ * @returns 
+ */
 export const removeTestTodoLists = async () => {
   return await TodoList.destroy({
     where: {
@@ -21,6 +34,11 @@ export const removeTestTodoLists = async () => {
   });
 };
 
+/**
+ * Add todo lists
+ * @param payload 
+ * @returns 
+ */
 export const addTestTodoList = async (payload: any) => {
   return await TodoList.create(payload);
 };
